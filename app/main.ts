6f5a11aa6db9d2393bda292ba3c0237be3921c04
Nikita Istomin <nikita.istomin54@gmail.com>
2025-07-05 02:25:25 -0500

    infinite pong

--- app/main.ts
+++ app/main.ts
@@ -2,7 +2,9 @@ import * as net from "net";
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
-  connection.end('+PONG\r\n')
+  while (true) {
+    connection.write('+PONG\r\n')
+  }
 });
 
 server.listen(6379, "127.0.0.1");
