d203ac7ca1a079a83154124cab3d621dbdf29cde
Nikita Istomin <nikita.istomin54@gmail.com>
2025-07-05 02:11:56 -0500

    pong

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,7 @@ import * as net from "net";
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
+  connection.end('+PONG\r\n')
 });
 
 server.listen(6379, "127.0.0.1");
